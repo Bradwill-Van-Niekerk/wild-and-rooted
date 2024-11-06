d0445a2c1fca3366cabe3f8a3bcbc6646310b083
Lami47 <liamrosenberg4777@gmail.com>
2024-11-06 15:00:54 +0200

    Orders template- in progress

---
+++ src/Pages-Staff/TempData.js
@@ -0,0 +1,35 @@
+export const OrderData = [
+    {
+        name: 'Johnny',
+        ClassName: 'box1',
+        subject: 'Biology/Life science',
+        Icon: 'https://i.postimg.cc/DysLKmML/icon.jpg',
+        Stars: 'https://i.postimg.cc/028P9Btn/n8dx1pd1.png',
+        src: 'https://www.youtube.com/embed/LMiCoo60XwM',
+        Reviewer: "Smitty",
+        ReviewContent: "This tutor is really good at explaining the breathing process, and covered in depth how the oxygen gets added into the bloodstream",
+        id: 1,
+    },
+    {
+        name: 'Kevin',
+        ClassName: 'box1',
+        subject: 'Maths and Sciences',
+        Icon: 'https://i.postimg.cc/DysLKmML/icon.jpg',
+        Stars: 'https://i.postimg.cc/028P9Btn/n8dx1pd1.png',
+        src: 'https://www.youtube.com/embed/3AJOGNaPH5I',
+        Reviewer: "Caitlin",
+        ReviewContent: "I had the pleasure of working with Kevin for the past 3 lesson. I've improve my understanding of Trigonmetry so much that i finally got over 70% for P2. From the very first session, it was clear that he is not only knowledgeable but also passionate about teaching math's.",
+        id: 2,
+    },
+    {
+        name: 'Lechuti',
+        ClassName: 'box1',
+        subject: 'English FAL',
+        Icon: 'https://i.postimg.cc/DysLKmML/icon.jpg',
+        Stars: 'https://i.postimg.cc/028P9Btn/n8dx1pd1.png',
+        src: 'https://www.youtube.com/embed/JYU5xIrjQQA',
+        Reviewer: "Emma",
+        ReviewContent: "Thanks to Lechuti, my English skills have improved significantly, both in writing and conversation. I feel more confident expressing myself and participating in discussions. The preparation for my exams went smoothly, and I achieved results I’m proud of. I highly recommend Lechuti to anyone seeking a dedicated and knowledgeable English FAL tutor. She truly has a gift for teaching",
+        id: 3,
+    }
+]
