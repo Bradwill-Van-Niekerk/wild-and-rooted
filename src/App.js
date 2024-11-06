bb9a5b8560fc24a880fdd8b2844815c1a99291db
khanya88 <khanyakwakhanya07@gmail.com>
2024-11-06 10:19:11 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import React from 'react';
 import Navbar from './Components/Navbar.js'
+import Home from './Components/Home';
+import Logo from './Pages/media/logo.jpeg';
+
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
     </div>
   );
 }
---
+++ src/Components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css';
+import logo from './Assets/images/logo.jpeg'; // Adjust path as needed
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <img src='src\Assets\images\logo.jpeg' alt="Farmhouse Fare Logo" className="hero-logo" />
+        <h1>Welcome to Farmhouse Fare</h1>
+        <p>Your farm-to-table experience starts here!</p>
+        <a href="#menu" className="btn">Explore Our Menu</a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
