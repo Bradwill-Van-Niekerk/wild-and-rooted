02db6d1c6677ec22de5903bb8fe0afb12e7256ee
Bradwill Van Niekerk <bradwill.oostendorp7@gmail.com>
2024-11-06 10:26:41 +0200

    update Links and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
 import Navbar from './Components/Navbar.js'
+import Inventory from './Pages-Staff/Inventory.js'
+
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/inventory' element={Inventory} />
+      {/* //   <Route path='/' element={} />
+      // <Route path='' element={} />
+      // <Route path='' element={} />
+      // <Route path='' element={} />
+      // <Route path='' element={} />
+      // <Route path='' element={} />
+      // <Route path='' element={} />
+      // <Route path='' element={} />
+      // <Route path='' element={} />
+      // <Route path='' element={} />
+      // <Route path='' element={} />
+      // <Route path='' element={} />
+      // <Route path='' element={} />
+      // <Route path='' element={} />
+      // <Route path='' element={} /> */}
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
